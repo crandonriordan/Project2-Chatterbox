adfd0e5368176d94e5787988f0f304af62448cde
crandonriordan <krillinmaster@gmail.com>
2018-09-27 11:51:20 -0400

    Attempting to solve CORS error through patch requests

--- p2-mvc/src/main/java/com/revature/controllers/PostController.java
+++ p2-mvc/src/main/java/com/revature/controllers/PostController.java
@@ -72,25 +72,7 @@ public class PostController {
 	
 //	
 //	END OF NEW NAMING CONVENTION
-	
-	@RequestMapping(value="/api/groups/{name}/posts", method={RequestMethod.HEAD, RequestMethod.OPTIONS, 
-			RequestMethod.PATCH, RequestMethod.TRACE})
-	public ResponseEntity notSupportedOnCorrectPathing() {
-		return new ResponseEntity(HttpStatus.OK);
-	}
-	
-	
-	@RequestMapping(value="/api/groups/{name}/posts/{id}", method={RequestMethod.HEAD, RequestMethod.OPTIONS, 
-			RequestMethod.PATCH, RequestMethod.TRACE})
-	public ResponseEntity notSupported() {
-		return new ResponseEntity(HttpStatus.OK);
-	}
-	
-	@RequestMapping(value="/api/posts/{name}", method={RequestMethod.HEAD, RequestMethod.OPTIONS, 
-			RequestMethod.PATCH, RequestMethod.TRACE})
-	public ResponseEntity notSupportedOld() {
-		return new ResponseEntity(HttpStatus.OK);
-	}
+
 	
 	
 	
