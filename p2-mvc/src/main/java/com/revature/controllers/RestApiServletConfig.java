a939efc8818772db1fc8d6b54661b621239596fd
crandonriordan <krillinmaster@gmail.com>
2018-09-26 22:31:00 -0400

    Fixing cors issues further

---
+++ p2-mvc/src/main/java/com/revature/controllers/RestApiServletConfig.java
@@ -0,0 +1,24 @@
+package com.revature.controllers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class RestApiServletConfig extends WebMvcConfigurerAdapter {
+
+//	fixes cors issues
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+    }
+}
--- p2-mvc/src/main/java/com/revature/controllers/UserController.java
+++ p2-mvc/src/main/java/com/revature/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +45,9 @@ public class UserController {
 		}
 		
 	}
+	
+//	@PutMapping(value="/api/groups/{id}/users/{userId}")
+//	public User addUserToGroup
 	
 	@DeleteMapping(value="/api/users/{id}", produces="application/json")
 	public User deleteUser(@RequestBody User user) {
